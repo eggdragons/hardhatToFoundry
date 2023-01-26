cad1d00d000b8e33547226c0aebaff19b933f253
eggdragon <dragoneggscollections@gmail.com>
2023-01-26 21:02:08 +0900

    add foundry & some sample tests

---
+++ foundry-test/Slot.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+
+contract SlotContract1 {
+    uint256 a;
+    uint256 b;
+    uint256 c;
+}
+
+contract SlotContract2 {
+    uint64 a;
+    uint64 b;
+    uint256 c;
+}
+
+contract SlotContract3 {
+    uint64 a;
+    uint256 b;
+    uint64 c;
+}
+
+//check forge inspect SlotContract1 storage-layout --pretty
+//check forge inspect SlotContract2 storage-layout --pretty
+//check forge inspect SlotContract3 storage-layout --pretty
